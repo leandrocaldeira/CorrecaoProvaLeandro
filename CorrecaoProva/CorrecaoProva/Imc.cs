d75c5d34dd0520c566900d0c666c4d20c4f45356
leandrocaldeira <leandrojudi@hotmail.com>
2018-10-16 20:30:39 -0300

    Add files via upload

---
+++ CorrecaoProva/CorrecaoProva/Imc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrecaoProva
+{
+    class Imc
+    {
+        private double peso;
+        private double altura;
+
+        public Imc()
+        {
+            this.peso = 0.0;
+            this.altura = 0.0;
+        }
+
+        public Imc (double peso, double altura)
+        {
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public double calcular (double peso, double altura)
+        {
+            return peso/ (altura*altura));
+        }
+
+        public double classificar()
+        {
+            return 0;
+        }
+    }
+}
---
+++ CorrecaoProva/CorrecaoProva/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrecaoProva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maior(1, 2));
+            Console.ReadKey();
+        }
+        static int Maior (int a, int b )
+        {
+            if (a > b)
+            {
+                return a;
+
+            }
+            else return b;
+        }
+        static bool Par (int num)
+        {
+            int resto = (num % 2 );
+            
+            if (resto ==0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
